95c518bd627e08cc0795598cda98d0283ab82d0b
dasunrandil <dasunrandil@gmail.com>
2022-09-05 17:19:37 +0530

    Close #8

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminSettingsFormController.java
@@ -0,0 +1,30 @@
+package lk.ijse.dep9.clinic.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminSettingsFormController {
+
+    public Button btnHospitalFee;
+    public Button btnDiscount;
+    public Button btnManageFields;
+    public Button btnPassword;
+    public Button btnAbout;
+    public AnchorPane anpField;
+
+    public void btnHospitalFeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDiscountOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageFieldsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+    }
+}
