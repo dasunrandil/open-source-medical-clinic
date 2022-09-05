46312cfd4b8847b2e238c335220e55fa4d5085ae
dasunrandil <dasunrandil@gmail.com>
2022-09-05 17:47:28 +0530

    Close #8

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/ManageFieldsFormController.java
@@ -0,0 +1,20 @@
+package lk.ijse.dep9.clinic.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class ManageFieldsFormController {
+
+    public Button btnNew;
+    public Button btnDelete;
+    public Button btnSave;
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+}
